2a2d323a2f57425371e54c140fc6a0e3242979d7
Tim Kim <95260439+timkimadobe@users.noreply.github.com>
2022-05-23 15:25:54 -0700

    Update UIWindowScene cast to guard statement

--- SampleApps/TestApp/SceneDelegate.swift
+++ SampleApps/TestApp/SceneDelegate.swift
@@ -17,12 +17,11 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: ContentView())
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: ContentView())
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
